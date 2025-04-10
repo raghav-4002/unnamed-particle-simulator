2b49cfdad3040ae13d6dace778aea4b7f94a59e6
Raghav Sharma <code.practice17@gmail.com>
2025-04-10 10:24:17 +0530

    Added comments

--- basic.c
+++ basic.c
@@ -20,6 +20,7 @@ kill(const char *message)
 void
 disable_raw_mode(void)
 {
+    /* during exit, restore the original terminal attributes */
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_attributes.orig_termios) == -1)
         kill("tcsetattr");
 
@@ -31,17 +32,21 @@ disable_raw_mode(void)
 void
 enable_raw_mode(void)
 {
+    /* get terminal attributes */
     if(tcgetattr(STDIN_FILENO, &term_attributes.orig_termios) == -1)
         kill("tcgetattr");
+    
     atexit(disable_raw_mode);
 
     struct termios raw;
     
+    /* settings to enable raw mode */
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN | CS8);
 	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
 
+    /* set the updated terminal attributes */
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
         kill("tcsetattr");
 }
@@ -52,6 +57,7 @@ get_window_size(void)
 {
     struct winsize ws;
 
+    /* using ioctl to get window size in the struct pointed by ws */
     if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1)
         kill("ioctl");
 
@@ -66,6 +72,7 @@ initialize(void)
     enable_raw_mode();
     get_window_size();
 
+    /* clear the screen */
     write(STDOUT_FILENO, "\x1b[2J", 4);
 
     /* hide cursor */
--- main.c
+++ main.c
@@ -23,7 +23,7 @@ main(int argc, char *argv[])
     unsigned particle_count = atol(argv[1]);
     Point particles[particle_count];
 
-    srand(time(NULL));
+    srand(time(NULL));  /* seed the random number generator */
     initialize();
 
     set_values(particles, particle_count);
@@ -55,12 +55,15 @@ draw(Point *point)
     char buf[32];
     int  len;
 
+    /* set cursor to new position */
     len = snprintf(buf, sizeof(buf), "\x1b[%d;%dH", 
             (int)point->position.y, (int)point->position.x);
-
     write(STDOUT_FILENO, buf, len);
+
+    /* draw the particle at the new cursor position */
     write(STDOUT_FILENO, "0", 1);
 
+    /* check for rebound from screen boundaries */
     rebound_from_edges(point);
 }
 
@@ -68,30 +71,27 @@ draw(Point *point)
 void 
 move(Point particles[], unsigned particle_count)
 {
-    const float req_frame_time = 1.0f / 40.0f;
+    const float req_frame_time = 1.0f / 40.0f;  /* target time for a single frame */
     struct timespec start, end;
     float elapsed_time, sleep_time;
 
     while(1) {
         clock_gettime(CLOCK_MONOTONIC, &start);
+        write(STDOUT_FILENO, "\x1b[2J", 4);     /* clear the screen */
 
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-
+        /* redraw the particles */
         for(unsigned i = 0; i < particle_count; i++) {
             update_parameters(&particles[i]);
-
             draw(&particles[i]);
         }
 
         clock_gettime(CLOCK_MONOTONIC, &end);
+        elapsed_time = (end.tv_nsec - start.tv_nsec) / (1.0e9);     /* get the actual time required to draw a frame */
+        sleep_time   = req_frame_time - elapsed_time;               /* difference b/w target and actual frame time */
 
-        elapsed_time = (end.tv_nsec - start.tv_nsec) / (1.0e9);
-
-        sleep_time   = req_frame_time - elapsed_time;
-
+        /* wait for some time if differnce is greater than zero */
         if(sleep_time > 0) {
             struct timespec ts = {0, sleep_time * (1.0e9)};
-
             nanosleep(&ts, NULL);
         }
     }
